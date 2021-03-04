349e4aa2d90cf2ad054d18136fc052a1a2177f56
Luissh22 <luisabraham_11@hotmail.com>
2021-03-03 20:09:15 -0500

    Add HTTP handler with health check endpoint

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,10 @@
 package main
 
-import "log"
+import (
+	transportHTTP "github.com/luissh22/rest-api/internal/transport/http"
+	"log"
+	"net/http"
+)
 
 type App struct {
 
@@ -8,6 +12,14 @@ type App struct {
 
 func (a *App) Run() error {
 	log.Println("Running application")
+	handler := transportHTTP.NewHandler()
+	handler.SetupRoutes()
+
+	if err := http.ListenAndServe(":8080", handler.Router); err != nil {
+		log.Println("Failed to setup server")
+		return err
+	}
+
 	return nil
 }
 
